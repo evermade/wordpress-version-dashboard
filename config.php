9a169dd362b94e90a70af0517ab44601a4523eaa
Mr Evermade <webmaster@evermade.fi>
2016-04-11 20:21:38 +0300

    Renamed sample config.#

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$apiKey = 'iusdofu2938x5478c93aEeqc5356dscfw';
-
-$config = array(
-	'test' => 'http://wp.onlyforprogrammer.com/',
-	'kampiapina' => 'http://www.kampiapina.com/blogi/',
-);
---
+++ config.sample.php
@@ -0,0 +1,8 @@
+<?php
+
+$apiKey = 'changethistosomethingelse';
+
+$config = array(
+	'officeanimals' => 'http://www.officanimals.com',
+	'kampiapina' => 'http://www.kampiapina.com',
+);
